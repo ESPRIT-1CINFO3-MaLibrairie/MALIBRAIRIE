d98301ded38a75f4fa9e3b07f10cd344651cb921
Ameni RAHMOUNI <ameni.rahmouni@esprit.tn>
2024-04-25 22:11:17 +0100

    creation des methodes de la classe Member

--- src/Member.java
+++ src/Member.java
@@ -2,4 +2,27 @@ public class Member {
     private String name;
     private int id;
 
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Method to display member details
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+    }
+
 }
