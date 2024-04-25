a515c5f51943b7ff4eaa68bee8ccdfd84f3ac737
Aziz Hajri <aziz.hajri@esprit.tn>
2024-04-25 22:06:59 +0100

    création getters & setters Ã

--- src/Book.java
+++ src/Book.java
@@ -10,6 +10,41 @@ public class Book {
         this.id = id;
         this.available = true; // Initially, book is available
     }
+    // Getters and setters
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+
+
     // Method to display book details
     public void displayDetails() {
         System.out.println("Title: " + title);
