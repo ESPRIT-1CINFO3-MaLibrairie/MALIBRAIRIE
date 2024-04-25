8c44d1d021b489d9ba19a1a8b4c297f7c87771a6
ines aouadi <aouadiines188@gmail.com>
2024-04-25 23:40:10 +0100

    creation desd library

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // Create a library
+        Library library = new Library();
+        library.addBook(book1);
+        library.addBook(book2);
     }
 }
