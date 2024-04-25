06f695215876376d938f9e6a50c082350d3bd045
ines aouadi <aouadiines188@gmail.com>
2024-04-25 22:05:36 +0100

    l'ajour du constructeur Library

--- src/Library.java
+++ src/Library.java
@@ -1,6 +1,12 @@
 package classes;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
     private List<Book> books;
+    // Constructor
+    public Library() {
+        this.books = new ArrayList<>();
+    }
+
 }
