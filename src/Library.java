46b370d166ba794638a26656e5ca3a46ed60aa6a
ines aouadi <aouadiines188@gmail.com>
2024-04-25 22:17:34 +0100

    l'ajout des methodes addBook and displayAllBooks

--- src/Library.java
+++ src/Library.java
@@ -8,5 +8,17 @@ public class Library {
     public Library() {
         this.books = new ArrayList<>();
     }
+    // Method to add a book to the library
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    // Method to display all books in the library
+    public void displayAllBooks() {
+        for (Book book : books) {
+            book.displayDetails();
+            System.out.println();
+        }
+    }
 
 }
