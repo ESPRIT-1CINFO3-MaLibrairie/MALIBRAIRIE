2e5f3f883a4c132f24e8b2a1c25b20c813dc33a6
Ameni RAHMOUNI <ameni.rahmouni@esprit.tn>
2024-04-25 23:17:09 +0100

    creation des book

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        // Create some books
+        Book book1 = new Book("Alice in borderland", "Bahe", 101);
+        Book book2 = new Book("Lupin", "Jihen", 102);
+
     }
 }
