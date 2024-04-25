82b2ea55acb61646cb96f0875b05eb696a4974cc
trabelsibahe <baha.trabelsi@gmail.com>
2024-04-25 21:50:56 +0100

    Création des attributs, création d'un nouveau constructeur Fournisseur.

--- src/fournisseur.java
+++ src/fournisseur.java
@@ -1,2 +1,20 @@
 public class fournisseur {
+
+    private String nom;
+    private String prenom;
+    private String email;
+
+    private int id;
+    private int cin;
+
+
+
+    // Constructor
+    public fournisseur(String nom, String prenom, String email, int id, int cin) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.id = id;
+        this.cin = cin;
+    }
 }
