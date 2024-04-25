7b8a48cff84d9ddffeb6a34e51c9dc22f4005664
Malek Moalla <malek.moalla@Proxitec.local>
2024-04-25 23:40:34 +0100

    Ajout des memebres

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 import java.io.PrintStream;
 public class Main {
     public static void main(String[] args) {
+// Create a member
+        Member member1 = new Member("Bahe", 1);
 
+        // Display member details
+        System.out.println("\n MEMBER DETAILS :\n");
+        member1.displayDetails();
         System.out.println("Hello world!");
 
 
